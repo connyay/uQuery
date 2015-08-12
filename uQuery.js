50515dbde0e668793a6dd4238e60508e2eb0e686
Connor Hindley <connor@redhat.com>
2015-08-11 23:54:37 -0400

    comments

--- uQuery.js
+++ uQuery.js
@@ -3,6 +3,12 @@
 
     var _forEach = Array.prototype.forEach;
 
+    /**
+     * Creates a new uQuery instance
+     * @constructor
+     * @param {string} selector - The css selector of elements
+     * @returns {uQuery}
+     */
     function uQuery(selector) {
         if (!(this instanceof uQuery)) {
             return new uQuery(selector);
@@ -10,22 +16,37 @@
         this._nodeList = document.querySelectorAll(selector);
     }
 
+    /**
+     * Makes the elements font color red
+     * @returns {uQuery}
+     */
     uQuery.prototype.makeRed = function () {
         return this._each(this._styleTransform('color', 'red'));
     };
 
+    /**
+     * Makes the elements font weight bold
+     * @returns {uQuery}
+     */
     uQuery.prototype.makeBold = function () {
         return this._each(this._styleTransform('fontWeight', 'bold'));
     };
 
+    /**
+     * Returns the value of the element(s)
+     * @returns {String|Array[String]}
+     */
     uQuery.prototype.getValue = function () {
+        // Empty nodelist situation
         if (!this._nodeList.length) {
             return null;
         }
+        // Single element in nodelist
         if (this._nodeList.length === 1) {
             var elem = this._nodeList[0];
             return elem.value || null;
         }
+        // Multiple elements in nodelist
         var results = [];
         this._each(function (e) {
             results.push(e.value);
